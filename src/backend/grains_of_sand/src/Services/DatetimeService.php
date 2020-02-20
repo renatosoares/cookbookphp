b7407dc8cd777fafd3cccb3592a21c6f0a4bbc46
Renato Soares <renatosoarespro@hotmail.com>
2020-02-20 09:37:05 -0300

    datetime working wiht intervals

--- src/backend/grains_of_sand/src/Services/DatetimeService.php
+++ src/backend/grains_of_sand/src/Services/DatetimeService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use DateInterval;
 use DateTime;
 use stdClass;
 
@@ -15,6 +16,32 @@ class DatetimeService
     public function custom(string $format, string $date): string
     {
         return date($format, strtotime($date));
+    }
+
+    public function addInterval(string $date, int $interval, string $spec, string $type): DateTime
+    {
+        $dateTime = new DateTime($date);
+
+        $intervalSpec = sprintf('%s%s%s',$spec, $interval, $type);
+
+        $interval = new DateInterval($intervalSpec);
+
+        return $dateTime->add($interval);
+    }
+
+    public function format(string $format, DateTime $dateTime): string
+    {
+        return $dateTime->format($format);
+    }
+
+    public function addMinutes(string $date, int $aditionalMinutes): DateTime
+    {
+        return $this->addInterval($date, $aditionalMinutes, 'PT', 'M');
+    }
+
+    public function addWeek(string $date, int $aditionalWeek): DateTime
+    {
+        return $this->addInterval($date, $aditionalWeek, 'P', 'W');
     }
 
     public function addDays(string $format, string $date, int $additionalDays): string
